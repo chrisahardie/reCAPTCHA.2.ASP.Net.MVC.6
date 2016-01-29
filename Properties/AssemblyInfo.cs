07c5f7a0b05515daf0c51d564a5f83406480f9ab
Chris Hardie <chrishardie@outlook.com>
2016-01-28 19:41:38 -0500

    Changed assembly name.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Recaptcha-dnxcore")]
+[assembly: AssemblyTitle("Recaptcha-dnx")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Next Designs Web Development Inc")]
-[assembly: AssemblyProduct("Recaptcha-dnxcore")]
+[assembly: AssemblyProduct("Recaptcha-dnx")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
